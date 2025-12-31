4a6bafe36d5891047b8b8040eb49273fc17a025f
Oliver Sim <160069657+AmericanSpirit38@users.noreply.github.com>
2025-12-31 17:36:02 +0100

    Removed test.py Added a command for current weather, forecast and exit

--- main.py
+++ main.py
@@ -57,6 +57,64 @@ def forecast(latitude, longitude, days=7):
         plt.show()
 
     CreateTemperaturePlot(hourly_dataframe)
+def current_weather(latitude, longitude):
+    url = "https://api.open-meteo.com/v1/forecast"
+    params = {
+        "latitude": latitude,
+        "longitude": longitude,
+        "current_weather": True
+    }
+
+    weather_codes = {
+        0: "Clear sky",
+        1: "Mainly clear",
+        2: "Partly cloudy",
+        3: "Overcast",
+        45: "Fog",
+        48: "Depositing rime fog",
+        51: "Light drizzle",
+        53: "Moderate drizzle",
+        55: "Dense drizzle",
+        56: "Light freezing drizzle",
+        57: "Dense freezing drizzle",
+        61: "Slight rain",
+        63: "Moderate rain",
+        65: "Heavy rain",
+        66: "Light freezing rain",
+        67: "Heavy freezing rain",
+        71: "Slight snow fall",
+        73: "Moderate snow fall",
+        75: "Heavy snow fall",
+        77: "Snow grains",
+        80: "Slight rain showers",
+        81: "Moderate rain showers",
+        82: "Violent rain showers",
+        85: "Slight snow showers",
+        86: "Heavy snow showers",
+        95: "Thunderstorm",
+        96: "Thunderstorm with slight hail",
+        99: "Thunderstorm with heavy hail"
+    }
+    response = requests.get(url, params=params)
+    response.raise_for_status()
+    data = response.json()
+
+    current = data["current_weather"]
+    print(weather_codes[current["weathercode"]])
+    print(f"{current['temperature']}°C")
+    print(f"{current['windspeed']}km/h with {current['winddirection']}° direction")
+    print(f"Elevation: {data['elevation']}m")
+    print(f"Time of measurement: {current["time"].split('T')[1]}")
+
+    url = "https://api.open-meteo.com/v1/forecast"
+    params = {
+        "latitude": latitude,
+        "longitude": longitude,
+        "forecast_days": 14,
+    }
+    response = requests.get(url, params=params)
+    response.raise_for_status()
+    data = response.json()
 
 print(colorama.Fore.WHITE + "Getting your location from your public IP..." + colorama.Style.RESET_ALL)
 response = requests.get("https://ipinfo.io/json")
@@ -82,6 +140,12 @@ while True:
                 forecast(latitude, longitude)
             except ValueError:
                 print(colorama.Fore.RED + "Days must be an integer between 1 and 14." + colorama.Style.RESET_ALL)
+        elif inp[0].lower() in ["exit", "quit", "q", "e"]:
+            print(colorama.Fore.WHITE + "Exiting..." + colorama.Style.RESET_ALL)
+            break
+        elif inp[0].lower() in ["current", "c"]:
+            print(colorama.Fore.WHITE + "Working..." + colorama.Style.RESET_ALL)
+            current_weather(latitude, longitude)
 
     except IndexError:
         print(colorama.Fore.RED + "Invalid input" + colorama.Style.RESET_ALL)
